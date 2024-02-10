6b90ea9ae6b1962c34a87dd43905b3db85f6205e
Yasemin <gunaly@umich.edu>
2024-02-09 23:48:42 -0500

    fixes

--- ps-3/main.js
+++ ps-3/main.js
@@ -1,50 +1,63 @@
 
-//const timeoutId = setTimeout(); //pass to clearTimeout(timeoutId) to clear
 let timer = false;
 let eventList = null;
-
+let selectedIndex = 0;
 
 getUMEventsWithImages((events) => {
     eventList = events;
-    console.log(eventList);
+    //console.log(eventList);
     const thumbDiv = document.querySelector("#thumbnails");
-
     for (let i=0; i<eventList.length; i++){
         let currEvent = events[i];
-        
-        console.log('')
         let newImg = document.createElement("img");
-        let eventDetails = document.create
-        newImg.src = currEvent['image_url']
+        newImg.setAttribute("id", `thumb-${currEvent.id}`);
+        newImg.src = currEvent.styled_images.event_thumb;
         thumbDiv.appendChild(newImg);
+
+        newImg.addEventListener('click', () => { 
+            setSelectedIndex(i);
+        });
+        
 
         //console.log(newImg.src);
     };
-
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        setSelectedIndex()
-    }, 10000);
+    setSelectedIndex(0);
+    // clearTimeout(timer);
+    // timer = setTimeout(() => {
+    //     selectedIndex = (selectedIndex + 1) % events.length;
+    //     setSelectedIndex(selectedIndex);
+    // }, 10000);
 
 });
 
-function setSelectedIndex() { 
-    for (let i = 0; i<eventList.length; i++){
-        let selectedIdx = eventList[i];
-        let selectedDate = document.querySelector('#selected-date')
-        let selectedTitleEl = document.querySelector('#selected-title')
-        let selectedDescrip = document.querySelector('#selected-description');
-        selectedTitleEl.value = selectedIdx['event_title'];
-        selectedTitleEl.src = selectedIdx['image_url'];
-        //selectedDate.value = getReadableTimes(selectedIdx['datetime_start']);
-        selectedDescrip.value = selectedIdx['description'];
-        //console.log("look here: " + selectedTitle);
-        //selectedIdx.classList.add('selected');
+function setSelectedIndex(i) { 
+    let prevSelected = document.querySelector(".selected");
+    if (prevSelected) { 
+        prevSelected.classList.remove('selected');
+    }
 
-        // give selected img title #selected-title 's value
+    const newSelect = document.querySelector(`#thumb-${eventList[i].id}`);
+    if (newSelect){
+        newSelect.classList.add('selected');
+    }
 
-        //give img at selectedIdx classList.add('selected')
-    };  
+    let selectedIdx = eventList[i]; 
+    //let selectedEl = document.querySelector(`#thumb-${selectedIdx}`);
+    let selectedDate = document.querySelector('#selected-date')
+    let selectedImage = document.querySelector('#selected-image')
+    let selectedTitleEl = document.querySelector('#selected-title')
+    let selectedDescrip = document.querySelector('#selected-description');
+    selectedTitleEl.textContent = selectedIdx.event_title;
+    selectedTitleEl.href = selectedIdx.permalink
+    selectedImage.src = selectedIdx.image_url;
+    selectedDate.textContent = getReadableTime(selectedIdx['datetime_start']);
+    selectedDescrip.textContent = selectedIdx['description'];
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        setSelectedIndex((i+1)%eventList.length);
+    }, 10000);
+
 };
 
 
