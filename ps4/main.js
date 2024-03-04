b540f01cd87952168ceec67a7d298c536a5400b8
Yasemin <gunaly@umich.edu>
2024-03-03 19:20:19 -0500

    start of ps4

---
+++ ps4/main.js
@@ -0,0 +1,42 @@
+/**
+ * Cache a fetch() request in localStorage and return the cached data if it's not expired.
+ * Useful if you are doing live editing refreshes and don't want to query the API every time.
+ * 
+ * @param {string} url The URL to fetch
+ * @param {*} options The options to pass to fetch()
+ * @param {number} cacheDuration The maximum age to use for cached data, in milliseconds
+ * @returns A Promise that resolves to a Response object
+ */
+function fetchWithCache(url, options = {}, cacheDuration = 1000 * 60 * 60) { // Default cache duration is 1 hour
+    // Utility function to create a Response object from data (like fetch() would)
+    function getResponseObject(data) {
+        return new Response(new Blob([JSON.stringify(data)]));
+    }
+
+    const cachedData = localStorage.getItem(url); // Check if we have cached data for this URL
+
+    if (cachedData) { // If we do...
+        const { timestamp, data } = JSON.parse(cachedData); // Parse the data from the cache
+        // Note: This uses destructuring syntax. It's equivalent to:
+        // const parsedCachedData = JSON.parse(cachedData);
+        // const timestamp = parsedCachedData.timestamp;
+        // const data = parsedCachedData.data;
+
+        if (Date.now() - timestamp < cacheDuration) { //...and it's not expired,
+            return Promise.resolve(getResponseObject(data)); // Return a promise whose value is the stored data
+        } else { // it has expired, so remove it
+            localStorage.removeItem(url);
+        }
+    }
+
+    // If we don't have cached data or it's expired, fetch it from the network
+    return fetch(url, options)
+        .then((response) => response.json()) // Parse the JSON data from the response
+        .then((data) => {
+            localStorage.setItem(url, JSON.stringify({ // Store the data in localStorage with a timestamp
+                timestamp: Date.now(),
+                data
+            }));
+            return getResponseObject(data);
+        });
+}
