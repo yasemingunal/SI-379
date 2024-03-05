be01563a352320d1a679155b26f9c18c616c5129
Yasemin <gunaly@umich.edu>
2024-03-05 14:23:35 -0500

    updates

--- ps4/main.js
+++ ps4/main.js
@@ -40,3 +40,72 @@ function fetchWithCache(url, options = {}, cacheDuration = 1000 * 60 * 60) { // 
             return getResponseObject(data);
         });
 }
+
+/**
+ * A function to randomly shuffle the items in an array and return a copy of the shuffled array.
+ * Based on: https://stackoverflow.com/a/12646864
+ * 
+ * @param {Array} array An array of any type
+ * @returns A shuffled copy of the array
+ */
+function shuffleArray(array) {
+    const shuffledArray = array.slice(); // Copy the array
+
+    // Shuffle the copy of the array using https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+    for (let i = shuffledArray.length - 1; i > 0; i--) { // For each index,
+        const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap elements i and j
+    }
+    return shuffledArray; // Return the shuffled copy
+}
+
+// const url = 'https://the-trivia-api.com/v2/questions';
+// const responsePromise = fetch(url);
+// constDataPromise = responsePromise.then((response) => {
+//     console.log(response);
+//     response.json().then((data)=>{
+//         console.log(data);
+//     })
+//     //return response.json();
+// });
+
+const quizElement = document.querySelector("#quiz");
+
+function getQuestionData(){
+    const finalVal = fetch('https://the-trivia-api.com/v2/questions').then(response => response.json());
+    console.log(finalVal);
+    return finalVal;
+    // const jsonPromise = promiseObject.then((value) =>{
+    //     console.log(value); // prints the response object
+    //     const jsonValue = value.json();
+    //     return jsonValue;
+    // });
+    // return jsonPromise.then((value) => {
+    //     console.log(value); // prints JSON Object
+    // })
+};
+
+//getQuestionData();
+
+function createQuiz(question, incorrectAnswers, correctAnswer){
+    const ul = document.createElement('ul');
+    const questionDisplay = document.createElement('a');
+    questionDisplay.innerText = question;
+    const allAnswers = incorrectAnswers.push(correctAnswer);
+    const shuffledAnswers = shuffleArray(allAnswers);
+    for(let answer in shuffledAnswers){
+        let li = document.createElement('li');
+        li.append(answer);
+        ul.append(li);
+    }
+
+}
+async function displayQuiz(){
+    const response = await getQuestionData();
+    for (const result of response.results) { 
+        const li = createQuiz(result.question, result.incorrectAnswers, result.correctAnswer);
+        quizElement.append(li);
+    }
+}
+
+displayQuiz();
