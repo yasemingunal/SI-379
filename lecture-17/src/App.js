5b47e088b9b769465ef9b63994c0f6745fb1356f
Yasemin <gunaly@umich.edu>
2024-03-19 23:32:25 -0400

    update

--- lecture-17/src/App.js
+++ lecture-17/src/App.js
@@ -1,36 +1,11 @@
 import Greeting from './Greeting';
 
 export default function App(){
+  const name = 'Yasemin';
+
   return <div>
-    <Greeting name = "Yasemin"/>
-    This is my first React application!
-    {/* This is my first react application! */}
+    {/* <Greeting name = "Yasemin" department='UMSI'/>
+    This is my first React application! */}
+    <button onClick = {() => console.log("Clicked")}>Click me</button>
     </div>;
 }
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello, world!</h1>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- ps6/src/App.js
+++ ps6/src/App.js
@@ -1,5 +1,7 @@
 import Slider from './Slider';
 import React from "react";
+import './ColorPicker.css';
+
 
 const MIN = 0;
 const MAX = 255;
@@ -9,26 +11,39 @@ function App() {
   const [green, setGreen] = React.useState(getRandomIntegerBetween(MIN, MAX));
   const [blue, setBlue]   = React.useState(getRandomIntegerBetween(MIN, MAX));
 
+  const [guessRed, setRedGuess] = React.useState(getRandomIntegerBetween(MIN, MAX));
+  const [guessGreen, setGreenGuess]  = React.useState(getRandomIntegerBetween(MIN, MAX));
+  const [guessBlue, setBlueGuess] = React.useState(getRandomIntegerBetween(MIN, MAX));
+
+  const redDiff = red - guessRed;
+
   return (
     <div className="App">
       <div id="color-preview" style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />
+      <p>Guess the color of the rectangle</p>
       <div id="color-picker">
         <div className="row">
-          <span className="component-color-preview" style={{backgroundColor: `rgb(255, 0, 0, ${red/MAX})`  }}>Red:</span>
-          <Slider min={MIN} max={MAX} startingValue={red} onChange={r => setRed(r)} />
+          <span className="component-color-preview" style={{backgroundColor: `rgb(255, 0, 0, ${guessRed/MAX})`  }}>Red:</span>
+          <Slider min={MIN} max={MAX} startingValue={guessRed} onChange={r => setRedGuess(r)} />
         </div>
         <div className="row">
-          <span className="component-color-preview" style={{backgroundColor: `rgb(0, 255, 0, ${green/MAX})`}}>Green:</span>
-          <Slider min={MIN} max={MAX} startingValue={green} onChange={g => setGreen(g)} />
+          <span className="component-color-preview" style={{backgroundColor: `rgb(0, 255, 0, ${guessGreen/MAX})`}}>Green:</span>
+          <Slider min={MIN} max={MAX} startingValue={guessGreen} onChange={g => setGreenGuess(g)} />
         </div>
         <div className="row">
-          <span className="component-color-preview" style={{backgroundColor: `rgb(0, 0, 255, ${blue/MAX})` }}>Blue:</span>
-          <Slider min={MIN} max={MAX} startingValue={blue} onChange={b => setBlue(b)} />
+          <span className="component-color-preview" style={{backgroundColor: `rgb(0, 0, 255, ${guessBlue/MAX})` }}>Blue:</span>
+          <Slider min={MIN} max={MAX} startingValue={guessBlue} onChange={b => setBlueGuess(b)} />
         </div>
       </div>
+      <button onClick={() => 
+        //<div id='guess-preview' style={{backgroundColor: `rgb(${guessRed}, ${guessGreen}, ${guessBlue})`}}></div>
+        console.log("Your guess: rgb("+ guessRed+", "+guessGreen+", "+guessBlue+")"+
+        "Actual: rgb("+ red+", "+green+", "+blue+")")
+      }>Guess</button>
     </div>
   );
 }
+
 
 export default App;
 
