7b7ec21c69717030b3baf5541a0df7473ac448cb
Yasemin <gunaly@umich.edu>
2024-03-20 14:18:45 -0400

    updates

--- ps6/src/App.js
+++ ps6/src/App.js
@@ -2,7 +2,6 @@ import Slider from './Slider';
 import React from "react";
 import './ColorPicker.css';
 
-
 const MIN = 0;
 const MAX = 255;
 
@@ -16,36 +15,85 @@ function App() {
   const [guessBlue, setBlueGuess] = React.useState(getRandomIntegerBetween(MIN, MAX));
 
   const [showingFeedback, setShowingFeedback] = React.useState(false);
+  const [cheatingMode, setCheatingMode] = React.useState(false);
 
+  const doGuess = React.useCallback(() => {
+    setShowingFeedback(true);
+  }, []);
 
-  const redDiff = red - guessRed;
+  const doAdvance = React.useCallback(() => {
+    setRed(getRandomIntegerBetween(MIN, MAX));
+    setGreen(getRandomIntegerBetween(MIN, MAX));
+    setBlue(getRandomIntegerBetween(MIN, MAX));
+
+    setRedGuess(MAX);
+    setGreenGuess(MAX);
+    setBlueGuess(MAX);
+
+    setShowingFeedback(false);
+  }, []);
+
+  const onKeyDown = React.useCallback((e) => {
+    if (e.key === "Enter") {
+      doGuess();
+    }
+  }, [doGuess]);
+
+  const onChangeCheatingMode = React.useCallback((e) => {
+    setCheatingMode(e.target.checked);
+  }, []);
+
+  const showUserColor = cheatingMode || showingFeedback;
 
   return (
     <div className="App">
+      <label id="cheating-mode">Cheating mode <input type="checkbox" value={cheatingMode} onChange={onChangeCheatingMode} /></label>
       <div id="color-preview" style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />
       <p>Guess the color of the rectangle</p>
+      
       <div id="color-picker">
         <div className="row">
           <span className="component-color-preview" style={{backgroundColor: `rgb(255, 0, 0, ${guessRed/MAX})`  }}>Red:</span>
-          <Slider min={MIN} max={MAX} startingValue={guessRed} onChange={r => setRedGuess(r)} />
+          
+          {!showingFeedback && <Slider min={MIN} max={MAX} startingValue={guessRed} onChange={r => setRedGuess(r)} onKeyDown={onKeyDown} />}
         </div>
         <div className="row">
+
           <span className="component-color-preview" style={{backgroundColor: `rgb(0, 255, 0, ${guessGreen/MAX})`}}>Green:</span>
-          <Slider min={MIN} max={MAX} startingValue={guessGreen} onChange={g => setGreenGuess(g)} />
+          
+          {!showingFeedback && <Slider min={MIN} max={MAX} startingValue={guessGreen} onChange={g => setGreenGuess(g)} onKeyDown={onKeyDown} />}
+        
         </div>
         <div className="row">
+          
           <span className="component-color-preview" style={{backgroundColor: `rgb(0, 0, 255, ${guessBlue/MAX})` }}>Blue:</span>
-          <Slider min={MIN} max={MAX} startingValue={guessBlue} onChange={b => setBlueGuess(b)} />
+          
+          {!showingFeedback && <Slider min={MIN} max={MAX} startingValue={guessBlue} onChange={b => setBlueGuess(b)}onKeyDown={onKeyDown} />}
+          
         </div>
+      </div>    
+
+      {showingFeedback && <p>Your guess: rgb({guessRed}, {guessGreen}, {guessBlue}). Actual: <strong>rgb({red}, {green}, {blue}).</strong></p> }
+
+      {!showingFeedback && <button onClick={doGuess}>Guess</button> }
+      {showingFeedback && <button onClick={doAdvance}>Next</button>}
+
+      <div id='guess-preview' style={{backgroundColor: `rgb(${guessRed}, ${guessGreen}, ${guessBlue})`}}/>
+      {showUserColor && <div id='answer-color' style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />}
       </div>
-      <button onClick={() => 
-        //<div id='guess-preview' style={{backgroundColor: `rgb(${guessRed}, ${guessGreen}, ${guessBlue})`}}></div>
-        console.log("Your guess: rgb("+ guessRed+", "+guessGreen+", "+guessBlue+")"+
-        "Actual: rgb("+ red+", "+green+", "+blue+")")
-      }>Guess</button>
-    </div>
   );
 }
+      /* {showingFeedback && 
+      (<div id='guess-preview' style={{backgroundColor: `rgb(${guessRed}, ${guessGreen}, ${guessBlue})`}}>
+      
+      </div>
+      )} */
+
+        /* //const newPreview = React.createElement('div', {className:'guessPreviewColor'}, 'The color you guessed: ' );
+        
+        // console.log("Your guess: rgb("+ guessRed+", "+guessGreen+", "+guessBlue+")"+
+        // "Actual: rgb("+ red+", "+green+", "+blue+")")
+      }>Guess</button> */
 
 
 export default App;
