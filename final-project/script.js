4bd923b280587f0f06f1294bc72d2cd0ae0cc69a
Yasemin <gunaly@umich.edu>
2024-03-25 22:14:58 -0400

    changes

--- final-project/script.js
+++ final-project/script.js
@@ -2,36 +2,38 @@
 const searchInput = document.querySelector("#searchBar");
 const searchInpVal = searchInput.value;
 const rotatingBottle = document.querySelector(".rotatingDrink");
-
 const searchButton = document.querySelector("#searchButton");
 
 
 //rotate the bottle animation: 
-
-setInterval(() => {
-    rotatingBottle.style.WebkitTransitionDuration="1s";
-    rotatingBottle.style.WebkitTransform='rotate(10deg)';
-    //rotatingBottle.style.WebkitTransform='rotate(-10deg)'
-}, 500);
-
-function getCocktailData(params){
-    const finalVal = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${params}`).then(response => response.json());
-    console.log(finalVal);
-    return finalVal;
+function rotateBottle(){
+    setInterval(() => { 
+        rotatingBottle.style.WebkitTransitionDuration="4s";
+        rotatingBottle.style.WebkitTransform='rotate(30deg)';
+    }, 500)
 };
 
-searchButton.addEventListener("click", () => {
-    console.log(searchInpVal)
-    console.log(getCocktailData(searchInpVal));
-    searchInpVal.value = ""; // clear search input 
-});
+rotateBottle();
 
-searchInput.addEventListener("keydown", (ev) => {
-    if (ev.key === 'Enter'){
-        console.log(getCocktailData(searchInpVal));
-    };
-    searchInpVal.value = ""; // clear search input 
-})
+
+// function getCocktailData(params){
+//     const finalVal = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${params}`).then(response => response.json());
+//     console.log(finalVal);
+//     searchInpVal.value = ""; // clear search input 
+//     return finalVal;
+// };
+
+// searchButton.addEventListener("click", () => {
+//     console.log(searchInpVal)
+//     console.log(getCocktailData(searchInpVal));
+// });
+
+// searchInput.addEventListener("keydown", (ev) => {
+//     if (ev.key === 'Enter'){
+//         console.log(getCocktailData(searchInpVal));
+//     };
+//     searchInpVal.value = ""; // clear search input 
+// })
 
 
 
