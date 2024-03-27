24eb78cd98797cae8073f35ff39c827ec30b3ed5
Yasemin <gunaly@umich.edu>
2024-03-27 15:57:52 -0400

    save

--- project-testing/script.js
+++ project-testing/script.js
@@ -1,86 +1,134 @@
 //import chroma from "chroma-js";
+// anime({
+//     targets: '.circle',
+//     translateX: 250
+// });
 
-const animationDiv = document.querySelector("svg");
-const fillButton = document.querySelector("#fillGlass");
-function easeOutQuad(t){return t*(2-t);};
-function easeInQuad(t){ return t*t;};
 
-function createMartiniGlass(){
-    const martiniTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    martiniTriangle.style.stroke = "black";
-    martiniTriangle.style.fill = 'white';
-    martiniTriangle.style.strokeWidth = '2';
-    animationDiv.appendChild(martiniTriangle);
+const quizButton = document.querySelector(".quizButton");
+const divEl = document.querySelector('#quiz')
 
-    const martiniLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    const martiniLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+document.addEventListener("DOMContentLoaded", () => {
+    quizButton.addEventListener("click", () =>{
+        console.log('quiz button clicked');
+        getCocktailData();
+        //console.log(fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`).then(response => response.json()))
+    });
+})
 
-    martiniLine1.setAttribute('x1', "100");
-    martiniLine1.setAttribute('y1', "50");
-    martiniLine1.setAttribute('x2', "100");
-    martiniLine1.setAttribute('y2', "100");
+//www.thecocktaildb.com/api/json/v1/1/search.php?s=
 
-    martiniLine2.setAttribute('x1', "80");
-    martiniLine2.setAttribute('y1', "100");
-    martiniLine2.setAttribute('x2', "120");
-    martiniLine2.setAttribute('y2', "100");
+// BELOW IS FOR API CALLING
+function getCocktailData(){
+    const margarita = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
+    .then(response => response.json()).then((data) => {
+        console.log(data.drinks[0].strDrink);
+    });
+    return margarita;
     
-    martiniLine1.style.strokeWidth='2';
-    martiniLine2.style.strokeWidth='2';
-    martiniLine1.style.stroke='black';
-    martiniLine2.style.stroke='black';
-    animationDiv.appendChild(martiniLine1);
-    animationDiv.appendChild(martiniLine2);
 
-    const array = arr = [ [ 150,0 ], [ 50,0 ],[ 100,50 ], ];
-    
-    for (let value of array) {
-      const point = animationDiv.createSVGPoint();
-      point.x = value[0];
-      point.y = value[1];
-      martiniTriangle.points.appendItem(point);
-    }
+    // const martini = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini`)
+    // .then(response => response.json()).then((data) => {
+    //     console.log(data.drinks[0].strDrink);
+    // });
+
+    // const negroni = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=negroni`)
+    // .then(response => response.json()).then((data) => {
+    //     console.log(data.drinks[0].strDrink);
+    // });
+
+    // const mojito = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito`)
+    // .then(response => response.json()).then((data) => {
+    //     console.log(data.drinks[0].strDrink);
+    // });
 }
 
-async function addGinToMartini(){
-    return new Promise((resolve) => {
-        const ginTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-        ginTriangle.style.fill = '#e6e3e3';
-        ginTriangle.setAttribute('opacity', 0);
 
-        //
-        const array = arr = [ [ 140, 10 ], [ 60,10 ],[ 100,50 ], ];
-        //[ [ 125, 25 ], [ 75,25 ],[ 100,50 ], ]
-        for (let value of array) {
-            const point = animationDiv.createSVGPoint();
-            point.x = value[0];
-            point.y = value[1];
-            ginTriangle.points.appendItem(point);
-        }
-        animationDiv.appendChild(ginTriangle);
 
-        const animationStarted = Date.now();
-        function step(){
-            const pct = (Date.now() - animationStarted) / 2000;
-            const pos = easeInQuad(pct);
-            ginTriangle.setAttribute('opacity', pos);
-            if (pct < 1){
-                requestAnimationFrame(step);
-            }
-        }
-        requestAnimationFrame(step);
-    });
-};
+// BELOW IS RELEVANT TO LEARN TO MIX PAGES:
 
+// const animationDiv = document.querySelector("svg");
+// const fillButton = document.querySelector("#fillGlass");
+// function easeOutQuad(t){return t*(2-t);};
+// function easeInQuad(t){ return t*t;};
+
+// function createMartiniGlass(){
+//     const martiniTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+//     martiniTriangle.style.stroke = "black";
+//     martiniTriangle.style.fill = 'white';
+//     martiniTriangle.style.strokeWidth = '2';
+//     animationDiv.appendChild(martiniTriangle);
+
+//     const martiniLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+//     const martiniLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+//     martiniLine1.setAttribute('x1', "100");
+//     martiniLine1.setAttribute('y1', "50");
+//     martiniLine1.setAttribute('x2', "100");
+//     martiniLine1.setAttribute('y2', "100");
+
+//     martiniLine2.setAttribute('x1', "80");
+//     martiniLine2.setAttribute('y1', "100");
+//     martiniLine2.setAttribute('x2', "120");
+//     martiniLine2.setAttribute('y2', "100");
     
+//     martiniLine1.style.strokeWidth='2';
+//     martiniLine2.style.strokeWidth='2';
+//     martiniLine1.style.stroke='black';
+//     martiniLine2.style.stroke='black';
+//     animationDiv.appendChild(martiniLine1);
+//     animationDiv.appendChild(martiniLine2);
 
-createMartiniGlass();
-fillButton.addEventListener("click", () => {
-    addGinToMartini();
-    const olive = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    olive.setAttribute('cx', "125");
-    olive.setAttribute('cy', "10");
-    olive.setAttribute('r', "5");
-    olive.setAttribute('fill', 'green');
-    animationDiv.append(olive);
-});
+//     const array = arr = [ [ 150,0 ], [ 50,0 ],[ 100,50 ], ];
+    
+//     for (let value of array) {
+//       const point = animationDiv.createSVGPoint();
+//       point.x = value[0];
+//       point.y = value[1];
+//       martiniTriangle.points.appendItem(point);
+//     }
+// }
+
+// async function addGinToMartini(){
+//     return new Promise((resolve) => {
+//         const ginTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+//         ginTriangle.style.fill = '#e6e3e3';
+//         ginTriangle.setAttribute('opacity', 0);
+
+//         //
+//         const array = arr = [ [ 140, 10 ], [ 60,10 ],[ 100,50 ], ];
+//         //[ [ 125, 25 ], [ 75,25 ],[ 100,50 ], ]
+//         for (let value of array) {
+//             const point = animationDiv.createSVGPoint();
+//             point.x = value[0];
+//             point.y = value[1];
+//             ginTriangle.points.appendItem(point);
+//         }
+//         animationDiv.appendChild(ginTriangle);
+
+//         const animationStarted = Date.now();
+//         function step(){
+//             const pct = (Date.now() - animationStarted) / 2000;
+//             const pos = easeInQuad(pct);
+//             ginTriangle.setAttribute('opacity', pos);
+//             if (pct < 1){
+//                 requestAnimationFrame(step);
+//             }
+//         }
+//         requestAnimationFrame(step);
+//     });
+// };
+
+// createMartiniGlass();
+// fillButton.addEventListener("click", () => {
+//     addGinToMartini();
+//     setTimeout(() => {
+//         const olive = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+//         olive.setAttribute('cx', "125");
+//         olive.setAttribute('cy', "10");
+//         olive.setAttribute('r', "5");
+//         olive.setAttribute('fill', 'green');
+//         animationDiv.append(olive);
+//     }, 2000)
+
+// });
