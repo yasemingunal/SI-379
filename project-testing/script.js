57958480eb1eebc0d539f134946bfb3f719fa69b
Yasemin <gunaly@umich.edu>
2024-04-06 14:24:28 -0400

    updates

--- project-testing/script.js
+++ project-testing/script.js
@@ -1,8 +1,9 @@
 //import chroma from "chroma-js";
 
 const quizButton = document.querySelector(".quizButton");
-//const learnButton = document.querySelector(".learnButton");
-const welcomeDiv = document.querySelector("#buttonCont");
+const learnButton = document.querySelector('.learnButton');
+learnButton.classList.add("disappear");
+const title = document.querySelector("#title");
 const learningSpan = document.querySelector("#learningContent");
 const learningCards = document.querySelector("#learningCards");
 const divEl = document.querySelector('#quiz');
@@ -13,27 +14,52 @@ question.textContent = "Which drink would you like to learn more about?";
 const searchBar = document.querySelector("#searchBar");
 searchBar.style.width = "300px";
 
+function easeOutQuad(t){return t*(2-t);};
+function easeInQuad(t){ return t*t;};
+function animate(el){
+    const animationStarted = Date.now();
+    function step(){
+        const pct = (Date.now() - animationStarted) / 1000;
+        const pos = easeInQuad(pct);
+        el.style.opacity = pos;
+        if (pct < 1){
+            requestAnimationFrame(step);
+        }
+    }
+    step();
+} 
+
 function fetchAppendDrinks() {
     const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-    const drinkNames = []
+    
     for (let i=0; i<4; i++){
         setTimeout(() => {
             fetch(apiUrl).then(response => response.json()).then(data => {
                 if (data.drinks && data.drinks.length > 0) {
-                    const drinkName = data.drinks[0].strDrink;
-                    drinkNames.push(drinkName);
+                    //console.log(data.drinks);
+                    let drinkTitle = document.createElement("p");
+                    let drinkImg = document.createElement("img");
+                    drinkImg.classList.add("drinkThumb");
+                    drinkImg.src = data.drinks[0].strDrinkThumb;
+                    drinkTitle.classList.add('drinkName');
+                    drinkTitle.innerText = data.drinks[0].strDrink;
                     const drinkElement = document.createElement('div');
                     drinkElement.classList.add('cardStyle');
-                    drinkElement.textContent = drinkName;
-                    drinkElement.style.transition = 'background-color 1s ease';
+                    drinkElement.append(drinkTitle);
+                    drinkElement.append(drinkImg);
                     drinkElement.setAttribute('id', 'randDrink');
+                    animate(drinkElement);
+                    animate(drinkImg);
                     drinkList.appendChild(drinkElement);
+                    let learnMoreButton = document.createElement('button');
+                    learnMoreButton.innerText = "Learn More";
+                    learnMoreButton.classList.add('button');
+                    learnMoreButton.setAttribute('id', 'learnMoreBut');
+                    drinkElement.append(learnMoreButton);
                 }
             });
         }, 2000 * i);
     }
-    console.log(drinkNames)
-    return drinkNames;
 };
 
 searchBar.addEventListener("keydown", (ev) => {
@@ -52,134 +78,57 @@ searchBar.addEventListener("keydown", (ev) => {
                 drinkTitle.classList.add('drinkName')
                 drinkTitle.innerText = data.drinks[0].strDrink;
                 drinkElement.append(drinkTitle);
-                //drinkElement.innerText = ;
-                ingredientsList.push(data.drinks[0].strIngredient1, data.drinks[0].strIngredient2, data.drinks[0].strIngredient3,
-                    data.drinks[0].strIngredient4);
-                }
-                for (let item in ingredientsList){
-                    let newLi = document.createElement('li');
-                    if (ingredientsList[item]){
-                        newLi.innerText = ingredientsList[item];
-                        drinkElement.append(newLi)
-                    }
-                    
-                };
+                let drinkImg = document.createElement("img");
+                drinkImg.classList.add("drinkThumb");
+                drinkImg.src = data.drinks[0].strDrinkThumb;
+                drinkElement.append(drinkImg);
+                drinkElement.setAttribute('id', 'randDrink');
+                animate(drinkElement);
+                animate(drinkImg);
+                // ingredientsList.push(data.drinks[0].strIngredient1, data.drinks[0].strIngredient2, data.drinks[0].strIngredient3,
+                //     data.drinks[0].strIngredient4);
+                // }
+                // for (let item in ingredientsList){
+                //     let newLi = document.createElement('li');
+                //     if (ingredientsList[item]){
+                //         newLi.innerText = ingredientsList[item];
+                //         drinkElement.append(newLi)
+                //     }
+                // };
                 let learnMoreButton = document.createElement('button');
                 learnMoreButton.innerText = "Learn More";
                 learnMoreButton.classList.add('button');
                 learnMoreButton.setAttribute('id', 'learnMoreBut');
                 drinkElement.append(learnMoreButton);
-                console.log("LOOK HERE: ", ingredientsList)
                 //drinkElement.append(ingredientsList);
                 learningSpan.append(drinkElement);
-
-            })
-        }
-        console.log(searchedDrink);
-
-    });
-// });
+            };
+        });
+    }});
 
 fetchAppendDrinks();
 
 
+// BUTTON FUNCTIONALITY:
+quizButton.addEventListener("click", () => {
+    learnButton.classList.remove("disappear");
+    learnButton.classList.add("appear");
+    quizButton.classList.remove('appear');
+    quizButton.classList.add('disappear');
+    learningSpan.classList.remove("appear");
+    learningSpan.classList.add("disappear");
+    title.classList.remove("disappear");
+    title.classList.add("appear");
+    title.innerText = "Select Quiz Settings:";
 
+});
 
-
-// learnButton.addEventListener('click', () => {
-     
-
-//     searchBar.classList.remove('disappear');
-//     searchBar.classList.add('appear');
-//     learningSpan.classList.add('questionDisplay');
-//     welcomeDiv.classList.add('disappear');
-
-// })
-
-// BELOW IS FOR "LEARN TO MIX" PAGES:
-
-// const animationDiv = document.querySelector("svg");
-// const fillButton = document.querySelector("#fillGlass");
-// function easeOutQuad(t){return t*(2-t);};
-// function easeInQuad(t){ return t*t;};
-
-// function createMartiniGlass(){
-//     const martiniTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-//     martiniTriangle.style.stroke = "black";
-//     martiniTriangle.style.fill = 'white';
-//     martiniTriangle.style.strokeWidth = '2';
-//     animationDiv.appendChild(martiniTriangle);
-
-//     const martiniLine1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-//     const martiniLine2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-
-//     martiniLine1.setAttribute('x1', "100");
-//     martiniLine1.setAttribute('y1', "50");
-//     martiniLine1.setAttribute('x2', "100");
-//     martiniLine1.setAttribute('y2', "100");
-
-//     martiniLine2.setAttribute('x1', "80");
-//     martiniLine2.setAttribute('y1', "100");
-//     martiniLine2.setAttribute('x2', "120");
-//     martiniLine2.setAttribute('y2', "100");
-    
-//     martiniLine1.style.strokeWidth='2';
-//     martiniLine2.style.strokeWidth='2';
-//     martiniLine1.style.stroke='black';
-//     martiniLine2.style.stroke='black';
-//     animationDiv.appendChild(martiniLine1);
-//     animationDiv.appendChild(martiniLine2);
-
-//     const array = arr = [ [ 150,0 ], [ 50,0 ],[ 100,50 ], ];
-    
-//     for (let value of array) {
-//       const point = animationDiv.createSVGPoint();
-//       point.x = value[0];
-//       point.y = value[1];
-//       martiniTriangle.points.appendItem(point);
-//     }
-// }
-
-// async function addGinToMartini(){
-//     return new Promise((resolve) => {
-//         const ginTriangle = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-//         ginTriangle.style.fill = '#e6e3e3';
-//         ginTriangle.setAttribute('opacity', 0);
-
-//         //
-//         const array = arr = [ [ 140, 10 ], [ 60,10 ],[ 100,50 ], ];
-//         //[ [ 125, 25 ], [ 75,25 ],[ 100,50 ], ]
-//         for (let value of array) {
-//             const point = animationDiv.createSVGPoint();
-//             point.x = value[0];
-//             point.y = value[1];
-//             ginTriangle.points.appendItem(point);
-//         }
-//         animationDiv.appendChild(ginTriangle);
-
-//         const animationStarted = Date.now();
-//         function step(){
-//             const pct = (Date.now() - animationStarted) / 2000;
-//             const pos = easeInQuad(pct);
-//             ginTriangle.setAttribute('opacity', pos);
-//             if (pct < 1){
-//                 requestAnimationFrame(step);
-//             }
-//         }
-//         requestAnimationFrame(step);
-//     });
-// };
-
-// createMartiniGlass();
-// fillButton.addEventListener("click", () => {
-//     addGinToMartini();
-//     setTimeout(() => {
-//         const olive = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-//         olive.setAttribute('cx', "125");
-//         olive.setAttribute('cy', "10");
-//         olive.setAttribute('r', "5");
-//         olive.setAttribute('fill', 'green');
-//         animationDiv.append(olive);
-//     }, 2000)
-
-// });
+learnButton.addEventListener("click", () => {
+    learnButton.classList.add("disappear");
+    learnButton.classList.remove("appear");
+    quizButton.classList.remove('disappear');
+    quizButton.classList.add('appear');
+    learningSpan.classList.remove('disappear');
+    learningSpan.classList.add('appear');
+    title.innerText = "Welcome!"
+});
