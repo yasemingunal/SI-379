3fde1c1d4fbc6724012312cfad876b03309543b9
Yasemin <gunaly@umich.edu>
2024-04-07 23:23:00 -0400

    update

--- finalprojectreact/src/App.js
+++ finalprojectreact/src/App.js
@@ -1,10 +1,16 @@
+
+
+import './App.css';
+
+let keys = ['C', 'C#', 'D', 'D#', 'E', 'F','F#', 'G', 'G#', 'A', 'A#', 'B', 'C'];
+// let keys2 = ['C#', 'D#', 'F#', 'G#', 'A#']
+
 function App() {
-  const [title = <div></div>]
-
-
   return (
-    <div>
-    </div>
+    <div className="piano">
+      {keys.map(key => (<div className='key'></div>))} 
+      {/* map works similar to for loops in plain JS */}
+        </div>
   );
 }
 
