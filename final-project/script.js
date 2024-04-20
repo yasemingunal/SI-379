7e70fddb32a8a7e514685e49b7739105c9c3cd6a
Yasemin <gunaly@umich.edu>
2024-04-20 13:26:11 -0400

    listen complete

--- final-project/script.js
+++ final-project/script.js
@@ -37,13 +37,18 @@ for (let key of keyDivs){
         const clickedKeyID = key.id;
         // key.classList.add('clicked');
         key.style.border = "2px red dashed";
+        key.classList.add('clicked');
         setTimeout(() => {
             key.style.border = 'black solid';
+            key.classList.remove('clicked');
             // key.classList.remove('clicked')
         }, 1000);
 
     console.log("clicked ", clickedKeyID)});
 }
+
+
+// let clickedKey = document.querySelector(".clicked");
 
 // LEARN TO PLAY: 
 learnButton.addEventListener('click', () => {
@@ -54,6 +59,20 @@ learnButton.addEventListener('click', () => {
     let instruction = document.createElement("p");
     let scoreNum = 0;
 
+    for (let key of keyDivs){
+        key.addEventListener("click", () => {
+            let clickedKey = key.id;
+            // key.classList.add('clicked');
+            key.style.border = "2px red dashed";
+            key.classList.add('clicked');
+            setTimeout(() => {
+                key.style.border = 'black solid';
+                key.classList.remove('clicked');
+                // key.classList.remove('clicked')
+            }, 1000);
+    
+        console.log("clicked ", clickedKey)});
+    }
 
     for (let i=0; i<10; i++){
         let quizNote = keyNames[Math.floor((Math.random()*keyNames.length))]
@@ -63,8 +82,15 @@ learnButton.addEventListener('click', () => {
 
         learningDiv.append(instruction);
         learningDiv.append(score);
+        console.log(clickedKey)
+        if (clickedKey == quizNote){
+            scoreNum++;
+            score.innerText = `Score: ${scoreNum}`;
+            console.log('correct');
+        } else {
+            console.log('incorrect');
+        }
     }
-    
     // for (let key of keyDivs){
     //     key.addEventListener("click", () => {
     //         const clickedKeyID = key.id;
@@ -92,21 +118,28 @@ listenButton.addEventListener("click", () => {
     //let furButton = document.createElement("button");
     let furTitle = document.createElement("h3");
     let clareTitle = document.createElement("h3");
+    let moonTitle = document.createElement("h3");
 
     let stopFurButton = document.createElement("button");
     let playFurButton = document.createElement("button");
     let stopClareButton = document.createElement("button");
     let playClareButton = document.createElement("button");
+    let stopMoonButton = document.createElement("button");
+    let playMoonButton = document.createElement("button");
 
     furTitle.innerHTML = "Fur Elise";
     furTitle.classList.add("songTitle");
     clareTitle.innerHTML = "Clare de Lune"
     clareTitle.classList.add("songTitle");
+    moonTitle.innerHTML = "Moonlight Sonata"
+    moonTitle.classList.add("songTitle");
 
     stopFurButton.innerHTML = 'Stop';
     playFurButton.innerHTML = "Play";
     stopClareButton.innerHTML = 'Stop';
     playClareButton.innerHTML = "Play";
+    stopMoonButton.innerHTML = 'Stop';
+    playMoonButton.innerHTML = "Play";
 
     listenDiv.append(furTitle);
     listenDiv.append(playFurButton);
@@ -114,9 +147,13 @@ listenButton.addEventListener("click", () => {
     listenDiv.append(clareTitle);
     listenDiv.append(playClareButton);
     listenDiv.append(stopClareButton);
+    listenDiv.append(moonTitle);
+    listenDiv.append(playMoonButton);
+    listenDiv.append(stopMoonButton);
 
     stopFurButton.disabled = true;
     stopClareButton.disabled = true;
+    stopMoonButton.disabled = true;
 
 
     playFurButton.addEventListener("click", () => {
@@ -142,13 +179,65 @@ listenButton.addEventListener("click", () => {
                 stopFurButton.disabled = true;
                 playFurButton.disabled = false;
             })
-
-        getData();
-    }})
+    }
+    getData();
+})
 
     playClareButton.addEventListener("click", () => {
         stopClareButton.disabled = false;
         playClareButton.disabled = true;
+        const clareUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=7ep7SSfuXsJAOW8CFTSJ2Q';
+        const options = {
+	    method: 'GET',
+	    headers: {
+		'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	    }
+    };
+        async function getClareData(){
+            const resp = await fetch(clareUrl, options);
+            const result2 = await resp.json();
+            console.log(result2['tracks'])
+            const clareLink = result2['tracks'][0]['preview_url'];
+            const clareAud = document.createElement("audio");
+            clareAud.src = clareLink;
+            clareAud.play();
+            stopClareButton.addEventListener("click", () => {
+                clareAud.pause();
+                stopClareButton.disabled = true;
+                playClareButton.disabled = false;
+            })
+        }
+        getClareData();
+
 })
-    
+
+    playMoonButton.addEventListener("click", () => {
+        stopMoonButton.disabled = false;
+        playMoonButton.disabled = true;
+
+        const moonUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=1j2M5ekufbxpzGYzuorgKt';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
+                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+            }
+        };
+        async function getMoonData(){
+            const resp2 = await fetch(moonUrl, options);
+            const result3 = await resp2.json();
+            console.log(result3['tracks'])
+            const moonLink = result3['tracks'][0]['preview_url'];
+            const moonAud = document.createElement("audio");
+            moonAud.src = moonLink;
+            moonAud.play();
+            stopMoonButton.addEventListener("click", () => {
+                moonAud.pause();
+                stopMoonButton.disabled = true;
+                playMoonButton.disabled = false;
+            })
+        }
+        getMoonData();
+    })
 });
