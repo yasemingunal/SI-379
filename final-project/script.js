8e102ef88e54ed525817efeeda871cd8622023c6
Yasemin <gunaly@umich.edu>
2024-04-24 14:58:18 -0400

    corrected code

--- final-project/script.js
+++ final-project/script.js
@@ -22,10 +22,14 @@ const freeplayButton = document.querySelector("#play");
 const distortButton = document.querySelector("#distort");
 const reverbButton = document.querySelector("#reverb");
 
-//learnButton.classList.add('disappear');
+let score = document.createElement("p");
+let instruction = document.createElement("h3");
+let feedback = document.createElement("p");
+
 listenButton.classList.add('disappear');
 distortButton.classList.add('disappear');
 reverbButton.classList.add('disappear');
+learnButton.classList.add('disappear')
 
 
 //screens activated via buttons:
@@ -71,7 +75,6 @@ async function reverbSound(sound){
     const audioEl = new Audio(sound);
     const source = audioContext.createMediaElementSource(audioEl);
     const convolver = audioContext.createConvolver();
-
     const response = await fetch(sound);
     const arrayBuffer = await response.arrayBuffer();
     const decodedAudio = await audioContext.decodeAudioData(arrayBuffer);
@@ -121,17 +124,20 @@ function playSound(note){
 }
 //normal sound: 
 freeplayButton.addEventListener("click", () => {
+    reverbButton.classList.remove('disappear');
+    distortButton.classList.remove('disappear');
+    reverbButton.classList.add('appear');
+    distortButton.classList.add('appear');
+    freeplayButton.classList.add('disappear');
+    let genInstruct = document.createElement('h3')
+    document.querySelector('.pianoInstruct').append(genInstruct);
     learningDiv.classList.remove('appear');
     learningDiv.classList.add('disappear');
-    listenDiv.classList.remove('appear');
-    listenDiv.classList.add('disappear');
 
     freeplayButton.classList.remove('appear');
     freeplayButton.classList.add('disappear');
     distortButton.classList.remove('disappear');
     distortButton.classList.add("appear");
-    listenButton.classList.remove('disappear');
-    listenButton.classList.add("appear");
     learnButton.classList.remove('disappear');
     learnButton.classList.add("appear");
     reverbButton.classList.remove('disappear');
@@ -162,23 +168,19 @@ freeplayButton.addEventListener("click", () => {
     }
 });
 
-
 // LEARN TO PLAY: 
 learnButton.addEventListener('click', () => {
-    // learnButton.classList.remove('appear');
-    // learnButton.classList.add('disappear');
-    learnButton.innerHTML = "Quiz me again";
+    distortButton.classList.add('disappear');
+    reverbButton.classList.add('disappear');
+    distortButton.classList.remove('appear');
+    reverbButton.classList.remove('appear');
+    learnButton.classList.remove('appear');
+    learnButton.classList.add('disappear');
     freeplayButton.classList.remove('disappear');
     freeplayButton.classList.add('appear');
-    listenDiv.classList.remove('appear');
-    listenDiv.classList.add('disappear');
     learningDiv.classList.remove('disappear');
     learningDiv.classList.add('appear');
-    
 
-    let score = document.createElement("p");
-    let instruction = document.createElement("p");
-    let feedback = document.createElement("p");
     let scoreNum = 0;
 
     let keysList = Object.keys(keyNames);
@@ -188,173 +190,178 @@ learnButton.addEventListener('click', () => {
     let correctKey = keyNames[quizNote];
     console.log("correctKey: ", correctKey)
 
-    instruction.innerText = `Play a ${keysList[randomIndex]}`;
-    feedback.innerText = "";
+
+    instruction.innerHTML = ""
+    instruction.innerHTML = `Click the ${keysList[randomIndex]} key`;
+    feedback.innerHTML = "";
 
     learningDiv.append(instruction);
     learningDiv.append(feedback);
-    for (let key of keyDivs){
+    learningDiv.append(score);
+
+    for (let key of keyDivs) {
         key.addEventListener('click', (ev) => {
-            console.log(keyNames2[correctKey]);
-            console.log(keyNames2[ev.target.id]);
-            if (keyNames2[ev.target.id] === keyNames2[correctKey]){
-                //console.log("clicked key: ", keyNames2[ev.target.id]);
-                feedback.innerHTML = "Nice!"
-                scoreNum++
+            if (key.id === correctKey) { 
+                feedback.innerHTML = "Nice!";
+                scoreNum++;
                 score.innerText = `Score: ${scoreNum}`;
-                learningDiv.append(feedback);
-                learningDiv.append(score);
-            } else{
+            } else {
                 feedback.innerHTML = `Not quite! That was a ${keyNames2[ev.target.id]}`;
-                score.innerText = `Score: ${scoreNum}`;
-                learningDiv.append(feedback);
-                learningDiv.append(score);
             }
+            // Generate a new question after each attempt
+            randomIndex = Math.floor(Math.random() * keysList.length);
+            quizNote = keysList[randomIndex];
+            correctKey = keyNames[quizNote];
+            instruction.innerText = `Click the ${quizNote} key`;
             learningDiv.append(feedback);
             learningDiv.append(score);
-            quizNote = "";
-            correctKey = "";
-        })
+        });
     }
-
 });
 
 
 // LISTEN FEATURE:
-listenButton.addEventListener("click", () => {
-    freeplayButton.classList.remove('disappear');
-    freeplayButton.classList.add('appear');
-    listenDiv.classList.remove('disappear');
-    listenDiv.classList.add('appear');
-    learningDiv.classList.remove('appear')
-    learningDiv.classList.add("disappear");
-    let listenInstructions = document.createElement("h1");
-    listenInstructions.innerHTML = "Select one of these pieces to listen to: ";
-    instructionsDiv.append(listenInstructions);
+let listenInstructions = document.createElement("h1");
+document.querySelector('.pianoInstruct').innerHTML = "";
+listenInstructions.innerHTML = "";
+freeplayButton.classList.remove('disappear');
+freeplayButton.classList.add('appear');
+distortButton.classList.remove('appear');
+reverbButton.classList.remove('appear');
+distortButton.classList.add('disappear');
+reverbButton.classList.add('disappear');
+listenDiv.classList.remove('disappear');
+listenDiv.classList.add('appear');
+learnButton.classList.remove('disappear');
+learnButton.classList.add('appear');
+learningDiv.classList.remove('appear')
+learningDiv.classList.add("disappear");
 
-    //get three song URLs, append them as buttons the listenDiv element 
-    let furTitle = document.createElement("h3");
-    let clareTitle = document.createElement("h3");
-    let moonTitle = document.createElement("h3");
+listenInstructions.innerHTML = "You can also select one of these pieces to listen to: ";
+listenDiv.append(listenInstructions);
 
-    let stopFurButton = document.createElement("button");
-    let playFurButton = document.createElement("button");
-    let stopClareButton = document.createElement("button");
-    let playClareButton = document.createElement("button");
-    let stopMoonButton = document.createElement("button");
-    let playMoonButton = document.createElement("button");
+//get three song URLs, append them as buttons the listenDiv element 
+let furTitle = document.createElement("h3");
+let clareTitle = document.createElement("h3");
+let moonTitle = document.createElement("h3");
 
-    furTitle.innerHTML = "Fur Elise";
-    furTitle.classList.add("songTitle");
-    clareTitle.innerHTML = "Clare de Lune"
-    clareTitle.classList.add("songTitle");
-    moonTitle.innerHTML = "Moonlight Sonata"
-    moonTitle.classList.add("songTitle");
+let stopFurButton = document.createElement("button");
+let playFurButton = document.createElement("button");
+let stopClareButton = document.createElement("button");
+let playClareButton = document.createElement("button");
+let stopMoonButton = document.createElement("button");
+let playMoonButton = document.createElement("button");
 
-    stopFurButton.innerHTML = 'Stop';
-    playFurButton.innerHTML = "Play";
-    stopClareButton.innerHTML = 'Stop';
-    playClareButton.innerHTML = "Play";
-    stopMoonButton.innerHTML = 'Stop';
-    playMoonButton.innerHTML = "Play";
+furTitle.innerHTML = "Fur Elise";
+furTitle.classList.add("songTitle");
+clareTitle.innerHTML = "Clare de Lune"
+clareTitle.classList.add("songTitle");
+moonTitle.innerHTML = "Moonlight Sonata"
+moonTitle.classList.add("songTitle");
 
-    listenDiv.append(furTitle);
-    listenDiv.append(playFurButton);
-    listenDiv.append(stopFurButton);
-    listenDiv.append(clareTitle);
-    listenDiv.append(playClareButton);
-    listenDiv.append(stopClareButton);
-    listenDiv.append(moonTitle);
-    listenDiv.append(playMoonButton);
-    listenDiv.append(stopMoonButton);
+stopFurButton.innerHTML = 'Stop';
+playFurButton.innerHTML = "Play";
+stopClareButton.innerHTML = 'Stop';
+playClareButton.innerHTML = "Play";
+stopMoonButton.innerHTML = 'Stop';
+playMoonButton.innerHTML = "Play";
 
-    stopFurButton.disabled = true;
-    stopClareButton.disabled = true;
-    stopMoonButton.disabled = true;
+listenDiv.append(furTitle);
+listenDiv.append(playFurButton);
+listenDiv.append(stopFurButton);
+listenDiv.append(clareTitle);
+listenDiv.append(playClareButton);
+listenDiv.append(stopClareButton);
+listenDiv.append(moonTitle);
+listenDiv.append(playMoonButton);
+listenDiv.append(stopMoonButton);
+
+stopFurButton.disabled = true;
+stopClareButton.disabled = true;
+stopMoonButton.disabled = true;
 
 
-    playFurButton.addEventListener("click", () => {
-        stopFurButton.disabled = false;
-        playFurButton.disabled = true;
-        const furUrl = 'https://spotify81.p.rapidapi.com/download_track?q=Fur%20Elise&onlyLinks=1';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
-                'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-            }
+playFurButton.addEventListener("click", () => {
+    stopFurButton.disabled = false;
+    playFurButton.disabled = true;
+    const furUrl = 'https://spotify81.p.rapidapi.com/download_track?q=Fur%20Elise&onlyLinks=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
         }
-        async function getData() {
-            const response = await fetch(furUrl, options);
-            const result = await response.json();
-            const url = result[0]['url'];
-            const furAud = document.createElement("audio");
-            furAud.src = url;
-            furAud.play();
-            stopFurButton.addEventListener("click", () =>{
-                furAud.pause();
-                stopFurButton.disabled = true;
-                playFurButton.disabled = false;
-            })
     }
-    getData();
+    async function getData() {
+        const response = await fetch(furUrl, options);
+        const result = await response.json();
+        const url = result[0]['url'];
+        const furAud = document.createElement("audio");
+        furAud.src = url;
+        furAud.play();
+        stopFurButton.addEventListener("click", () =>{
+            furAud.pause();
+            stopFurButton.disabled = true;
+            playFurButton.disabled = false;
+        })
+}
+getData();
 })
 
-    playClareButton.addEventListener("click", () => {
-        stopClareButton.disabled = false;
-        playClareButton.disabled = true;
-        const clareUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=7ep7SSfuXsJAOW8CFTSJ2Q';
-        const options = {
-	    method: 'GET',
-	    headers: {
-		'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	    }
+playClareButton.addEventListener("click", () => {
+    stopClareButton.disabled = false;
+    playClareButton.disabled = true;
+    const clareUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=7ep7SSfuXsJAOW8CFTSJ2Q';
+    const options = {
+    method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
+    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+};
+    async function getClareData(){
+        const resp = await fetch(clareUrl, options);
+        const result2 = await resp.json();
+        console.log(result2['tracks'])
+        const clareLink = result2['tracks'][0]['preview_url'];
+        const clareAud = document.createElement("audio");
+        clareAud.src = clareLink;
+        clareAud.play();
+        stopClareButton.addEventListener("click", () => {
+            clareAud.pause();
+            stopClareButton.disabled = true;
+            playClareButton.disabled = false;
+        })
+    }
+    getClareData();
+
+})
+
+playMoonButton.addEventListener("click", () => {
+    stopMoonButton.disabled = false;
+    playMoonButton.disabled = true;
+
+    const moonUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=1j2M5ekufbxpzGYzuorgKt';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
     };
-        async function getClareData(){
-            const resp = await fetch(clareUrl, options);
-            const result2 = await resp.json();
-            console.log(result2['tracks'])
-            const clareLink = result2['tracks'][0]['preview_url'];
-            const clareAud = document.createElement("audio");
-            clareAud.src = clareLink;
-            clareAud.play();
-            stopClareButton.addEventListener("click", () => {
-                clareAud.pause();
-                stopClareButton.disabled = true;
-                playClareButton.disabled = false;
-            })
-        }
-        getClareData();
-
-})
-
-    playMoonButton.addEventListener("click", () => {
-        stopMoonButton.disabled = false;
-        playMoonButton.disabled = true;
-
-        const moonUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=1j2M5ekufbxpzGYzuorgKt';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'd8b3104eb9msh3ecd4325e9378b6p1b25e3jsn9fec64e3cc2c',
-                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-            }
-        };
-        async function getMoonData(){
-            const resp2 = await fetch(moonUrl, options);
-            const result3 = await resp2.json();
-            console.log(result3['tracks'])
-            const moonLink = result3['tracks'][0]['preview_url'];
-            const moonAud = document.createElement("audio");
-            moonAud.src = moonLink;
-            moonAud.play();
-            stopMoonButton.addEventListener("click", () => {
-                moonAud.pause();
-                stopMoonButton.disabled = true;
-                playMoonButton.disabled = false;
-            })
-        }
-        getMoonData();
-    })
+    async function getMoonData(){
+        const resp2 = await fetch(moonUrl, options);
+        const result3 = await resp2.json();
+        console.log(result3['tracks'])
+        const moonLink = result3['tracks'][0]['preview_url'];
+        const moonAud = document.createElement("audio");
+        moonAud.src = moonLink;
+        moonAud.play();
+        stopMoonButton.addEventListener("click", () => {
+            moonAud.pause();
+            stopMoonButton.disabled = true;
+            playMoonButton.disabled = false;
+        })
+    }
+    getMoonData();
 });
